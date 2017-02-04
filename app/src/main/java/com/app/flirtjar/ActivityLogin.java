bfc9c13e086625201733013ef4215395556eaca4
rutvik <rutvik106@live.com>
2017-02-04 20:09:53 +0530

    @Home, committing

--- app/src/main/java/com/app/flirtjar/ActivityLogin.java
+++ app/src/main/java/com/app/flirtjar/ActivityLogin.java
@@ -1,8 +1,23 @@
 package com.app.flirtjar;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -10,19 +25,73 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ActivityLogin extends AppCompatActivity implements FacebookCallback<LoginResult>
+
+public class ActivityLogin extends Activity implements FacebookCallback<LoginResult>
 {
 
     @BindView(R.id.btn_fbLogin)
     LoginButton btnFbLogin;
 
+    @BindView(R.id.tv_lbl)
+    TextView tv_lbl;
+
+
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+
+    @BindView(R.id.layoutDots)
+    LinearLayout dotsLayout;
+
+    @BindView(R.id.btn_next)
+    Button btnNext;
     CallbackManager callbackManager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private TextView[] dots;
+    private int[] layouts;
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener()
+    {
+
+        @Override
+        public void onPageSelected(int position)
+        {
+            addBottomDots(position);
+
+            // changing the next button text 'NEXT' / 'GOT IT'
+            if (position == layouts.length - 1)
+            {
+                // last page. make button text to GOT IT
+                btnNext.setText(getString(R.string.start));
+                //btnSkip.setVisibility(View.GONE);
+            } else
+            {
+                // still pages are left
+                btnNext.setText(getString(R.string.next));
+                //btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2)
+        {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0)
+        {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,6 +100,95 @@ public class ActivityLogin extends AppCompatActivity implements FacebookCallback
         setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
+
+
+        // Hide Status Bar
+        if (Build.VERSION.SDK_INT < 16)
+        {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else
+        {
+            View decorView = getWindow().getDecorView();
+            // Hide Status Bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+
+
+        PermissionListener permissionlistener = new PermissionListener()
+        {
+            @Override
+            public void onPermissionGranted()
+            {
+
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions)
+            {
+                Log.d("DENIED PERMISSION", deniedPermissions.toString());
+            }
+
+
+        };
+        new TedPermission(this)
+                .setPermissionListener(permissionlistener)
+                .setPermissions(Manifest.permission.CALL_PHONE,
+                        Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.NFC)
+                .check();
+
+
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = new int[]{
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2,
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2};
+
+        // adding bottom dots
+        addBottomDots(0);
+
+        // making notification bar transparent
+        //        changeStatusBarColor();
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+
+        btnNext.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // checking for last page
+                // if last page home screen will be launched
+                int current = getItem(+1);
+                if (current < layouts.length)
+                {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);
+                } else
+                {
+                    //                    launchHomeScreen();
+                }
+            }
+        });
+        Typeface face = Typeface.createFromAsset(getAssets(), "font/Pacifico-Regular.ttf");
+        tv_lbl.setTypeface(face);
+
+        btnFbLogin.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(ActivityLogin.this, "CLICKED", Toast.LENGTH_LONG).show();
+            }
+        });
 
         callbackManager = CallbackManager.Factory.create();
 
@@ -47,8 +205,35 @@ public class ActivityLogin extends AppCompatActivity implements FacebookCallback
 
         btnFbLogin.registerCallback(callbackManager, this);
 
-        btnFbLogin.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
 
+    }
+
+    private void addBottomDots(int currentPage)
+    {
+        dots = new TextView[layouts.length];
+
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++)
+        {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+        {
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+        }
+    }
+
+    private int getItem(int i)
+    {
+        return viewPager.getCurrentItem() + i;
     }
 
     @Override
@@ -77,5 +262,49 @@ public class ActivityLogin extends AppCompatActivity implements FacebookCallback
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
+    /**
+     * View pager adapter
+     */
+    public class MyViewPagerAdapter extends PagerAdapter
+    {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter()
+        {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position)
+        {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount()
+        {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj)
+        {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object)
+        {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+
 
 }
